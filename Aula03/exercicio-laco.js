e8f02fe518b3226f2d4774c83400da13c9d014fb
Bruno Groppo <bmgroppo@gmail.com>
2025-05-22 15:57:34 -0300

    continuar

--- Aula03/exercicio-laco.js
+++ Aula03/exercicio-laco.js
@@ -1,8 +1,19 @@
 //Exercicio
 //01 Crie um programa que receba 6 números e no final apresente a média destes números.
+let numero = 0;
+let media = 0;
+let mediaTotal = 0;
+let contador = 0;
+while ( contador < media) {
+    numero = Number(prompt(`Digite o ${contador + 1}º número de ${media} escolhidos:`));
+    if (!isNaN(numero)) {   
+       //arrumar a média
 
-
-
+    }
+    else {
+        alert("Valor inválido. Tente novamente.");
+    }
+}
 
 //02 Crie um programa que receba 10 números e no final mostra a quantidade de números pares e ímpares digitados.
 
