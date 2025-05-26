3ae68618ed00b81c3432021a91e5ae629da0d035
Bruno Groppo <bmgroppo@gmail.com>
2025-05-26 09:09:09 -0300

    Exercicio

--- Aula03/exercicio-laco.js
+++ Aula03/exercicio-laco.js
@@ -1,20 +1,6 @@
 //Exercicio
 //01 Crie um programa que receba 6 números e no final apresente a média destes números.
-let numero = 0;
-let media = 0;
-let mediaTotal = 0;
-let contador = 0;
-while ( contador < media) {
-    numero = Number(prompt(`Digite o ${contador + 1}º número de ${media} escolhidos:`));
-    if (!isNaN(numero)) {   
-       //arrumar a média
-
-    }
-    else {
-        alert("Valor inválido. Tente novamente.");
-    }
-}
-
+Exercicio1.html
 //02 Crie um programa que receba 10 números e no final mostra a quantidade de números pares e ímpares digitados.
 
 //03 Usando o laço for, crie um programa que receba um número do usuário e mostre a tabuada deste número. Exemplo: 2 x 1 = 2, 2 x 2 = 4, ..., 2 x 10 = 20
