ab5edbe9ee577caff6544479492bc04f949ba889
Bruno Groppo <bmgroppo@gmail.com>
2025-05-26 19:21:17 -0300

    Finalizado

--- Exercícios/lista1.js
+++ Exercícios/lista1.js
@@ -40,3 +40,37 @@ function exercicio5() {
     let celsius = (fahrenheit - 32) * 5 / 9;
     document.writeln(`A temperatura em Celsius é: ${celsius.toFixed(2)}°C <br>`);
 }
+
+function exercicio6(){
+    let num1 = Number(prompt("Digite o primeiro número"));
+    let num2 = Number(prompt("Digite o segundo número"));
+    let num3 = Number(prompt("Digite o terceiro numero"));
+    let media = (num1+num2+num3)/3
+
+    document.writeln(`A média aritimética de ${num1}, ${num2} e ${num3} é ${media.toFixed(2)}`);
+}
+
+function exercicio7(){
+    let distancia = Number(prompt("Digite a distancia percorrida"));
+    let consumo = Number(prompt("Digite quantos kilometros o automóvel faz por litro (km/l)"))
+    let litros = consumo/distancia
+
+    document.writeln(`O automóvel precisa de ${litros.toFixed(2)} litros de combustível para percorrer ${distancia} kilometros`)
+}
+
+function exercicio8(){
+    let num = Number(prompt("Digite um número"));
+    let quadrado = num**2;
+    let raiz = num**0.5;
+
+    document.writeln(`O número é ${num}, o quadrado de ${num} é ${quadrado.toFixed(2)} e a raíz quadrada de ${num} é ${raiz.toFixed(2)}`)
+}
+
+function exercicio9(){
+    let prestacao = Number(prompt("Digite o valor original da prestação"));
+    let diasAtraso = Number(prompt("Digite quantos dias a prestação está atrasada"));
+    let taxa = Number(prompt("Digite qual a taxa de juros por dia"));
+    let atual = (prestacao*((taxa/100)*diasAtraso))+prestacao;
+    
+    document.writeln(`A prestação atualizada hoje é R$ ${atual.toFixed(2)}`)
+}
